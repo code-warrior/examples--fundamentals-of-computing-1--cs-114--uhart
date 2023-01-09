0c130d1623edac092e71f9b85cc051bab553e97c
Roy Vanegas <roy@thecodeeducators.com>
2023-01-09 16:16:59 -0500

    Add MiniQuiz example from chapter 7

---
+++ textbook-examples/chapter-7-examples/MiniQuiz/Complexity.java
@@ -0,0 +1,11 @@
+// *****************************************************************************
+// Complexity.java       Author: Lewis/Loftus
+//
+// Represents the interface for an object that can be assigned an explicit
+// complexity.
+// *****************************************************************************
+
+public interface Complexity {
+  public void setComplexity(int complexity);
+  public int getComplexity();
+}
