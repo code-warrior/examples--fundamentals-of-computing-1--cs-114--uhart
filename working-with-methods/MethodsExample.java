0b1217ae508355d716081fa6bd9c305dedc02442
Roy A Vanegas <roy@thecodeeducators.com>
2023-02-27 17:11:15 -0500

    Add methods example

---
+++ working-with-methods/MethodsExample.java
@@ -0,0 +1,24 @@
+import javax.print.attribute.standard.PrinterName;
+
+public class MethodsExample {
+  public static void printName() {
+    System.out.printf("%n%s%n", "Ed Bob");
+  }
+
+  public static String getName() {
+    return "EdBob";
+  }
+
+  public static int getSum(int x, int y) {
+    return x + y;
+  }
+
+  public static void main(String[] args) {
+    int x = 20;
+    int y = 40;
+
+    printName();
+    System.out.printf("The cat’s name is %s%n", getName());
+    System.out.printf("The sum of %d and %d is %d%n", x, y, getSum(x, y));
+  }
+}
