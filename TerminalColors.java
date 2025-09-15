d83ad88acb0f1232ca19fc6f10f5035ab69ff042
Roy Vanegas <roy@thecodeeducators.com>
2025-09-15 06:57:06 -0400

    Add comment

--- TerminalColors.java
+++ TerminalColors.java
@@ -1,4 +1,11 @@
 public class TerminalColors {
+  /*
+   * See the following Stack Overflow entry for a more detailed discussion about
+   * rendering color in your command line interface:
+   *
+   * https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+   *
+   */
   public static void main(String[] args) {
     final String RESET = "\u001b[0m";
     final String HIGHLIGHT = "\u001b[33m";
