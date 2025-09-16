63e2a35d7ee38b1e2409b4dd3898bc3ab5bd88e9
Roy Vanegas <roy@thecodeeducators.com>
2025-09-16 17:47:10 -0400

    Add PickOffCharactersFromString.java

---
+++ working-with-strings/PickOffCharactersFromString.java
@@ -0,0 +1,16 @@
+public class PickOffCharactersFromString {
+  public static void main(String[] args) {
+    String band = "Bad Brains";
+
+    System.out.println(band.charAt(0));
+    System.out.println(band.charAt(1));
+    System.out.println(band.charAt(2));
+    System.out.println(band.charAt(3));
+    System.out.println(band.charAt(4));
+    System.out.println(band.charAt(5));
+    System.out.println(band.charAt(6));
+    System.out.println(band.charAt(7));
+    System.out.println(band.charAt(8));
+    System.out.println(band.charAt(9));
+  }
+}
