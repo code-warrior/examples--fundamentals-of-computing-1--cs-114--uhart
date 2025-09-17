8976925f51250082615a31902447c6bffb69d661
Roy A Vanegas <roy@thecodeeducators.com>
2025-09-17 13:54:13 -0400

    Add MaxValuesForNumericPrimitives.java

---
+++ MaxValuesForNumericPrimitives.java
@@ -0,0 +1,20 @@
+public class MaxValuesForNumericPrimitives {
+  public static void main(String[] args) {
+    // Remember BaSIL For Dressing, or BaSIL FD (the letter “a” doesn’t stand for anything):
+    //
+    // byte, short, integer, long, float, and double
+    //
+    System.out.println("A " + CLIColors.RED + "byte" + CLIColors.NORMAL + "’s max value is " + CLIColors.RED + Byte.MAX_VALUE + CLIColors.NORMAL);
+    System.out.println();
+    System.out.println("A " + CLIColors.CYAN + "short" + CLIColors.NORMAL + "’s max value is " + CLIColors.CYAN + Short.MAX_VALUE + CLIColors.NORMAL);
+    System.out.println();
+    System.out.println("An " + CLIColors.GREEN + "int" + CLIColors.NORMAL + "’s max value is " + CLIColors.GREEN + Integer.MAX_VALUE + CLIColors.NORMAL);
+    System.out.println();
+    System.out.println("An " + CLIColors.BLUE + "long" + CLIColors.NORMAL + "’s max value is " + CLIColors.BLUE + Integer.MAX_VALUE + CLIColors.NORMAL);
+    System.out.println();
+    System.out.println("A " + CLIColors.YELLOW + "float" + CLIColors.NORMAL + "’s max value is " + CLIColors.YELLOW + Float.MAX_VALUE + CLIColors.NORMAL);
+    System.out.println();
+    System.out.println("A " + CLIColors.MAGENTA + "double" + CLIColors.NORMAL + "’s max value is " + CLIColors.MAGENTA + Double.MAX_VALUE + CLIColors.NORMAL);
+    System.out.println();
+  }
+}
