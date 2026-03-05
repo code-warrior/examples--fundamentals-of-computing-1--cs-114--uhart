03e7d41d494340997dae3627c0bedf72fa4bed37
Roy A Vanegas <roy@thecodeeducators.com>
2026-03-05 16:38:08 -0500

    Add ForExample.java

---
+++ programming-structures/loops/the-for-loop/ForExample.java
@@ -0,0 +1,9 @@
+public class ForExample {
+  public static void main(String[] args) {
+    final int MAX = 5;
+
+    for(int index = 0; index < MAX; index++) {
+      System.out.println("index is " + index);
+    }
+  }
+}
